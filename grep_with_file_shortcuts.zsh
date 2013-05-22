f755cbce39d774652cb803726522fb918667312e
Janne Koponen <janne.koponen@wunderkraut.com>
2013-05-22 16:22:20 +0300

    Clear linenumber code out.

--- grep_with_file_shortcuts.zsh
+++ grep_with_file_shortcuts.zsh
@@ -1,17 +1,6 @@
 #!/usr/bin/env zsh
 function grep_with_file_shortcuts()
 {
-  #appendlinenumber=0
-  #while getopts "v:" option
-  #do
-    #case $option in
-      #v)
-        #appendlinenumber=1
-        #(( $# = $# - 1 ))
-        #;;
-  #esac
-  #done
-
   if [[ "$#" -eq 1 ]]; then
     results=(${(f)"$(grep -rn $1)"} )
   else
@@ -27,11 +16,6 @@ function grep_with_file_shortcuts()
       file_path=(${line%%:*}(:A))
     fi
     e="e$i"
-    #v="v$i"
-    #if [[ appendlinenumber -eq 1 ]]; then
-      #line_number="+${${line#*:}%:*}"
-      #export $v="line_number"
-    #fi
     export $e="$file_path"
     (( i = i + 1 ))
   done
