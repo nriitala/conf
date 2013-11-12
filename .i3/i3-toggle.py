e47d24a4465e973a89aac4312fd445c1af6e33b7
nriitala <nri@iki.fi>
2013-11-12 15:53:28 +0200

    some i3 python scripts added

---
+++ .i3/i3-toggle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import i3
+
+"""
+Requirements: https://github.com/ziberna/i3-py
+"""
+workspaces = i3.get_workspaces()
+
+for workspace in workspaces:
+  if (workspace['focused'] == True):
+    if (workspace['num'] == 1):
+      i3.workspace10()
+    else:
+      i3.workspace1()
