7f095bbf39aa9b1253b857fe2a495bdd37ac4ee9
nriitala <nri@iki.fi>
2014-10-21 10:02:00 +0300

    added drush_site_audit script

---
+++ scripts/drush_site_audit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+command -v drush >/dev/null 2>&1 || { echo '$ drush is not in $PATH. Aborting.' >&2; exit 1; }
+if [ ! -e "$HOME/.drush/site_audit" ]
+then
+    echo "You must install drush site_audit first, see"
+    echo "https://www.drupal.org/project/site_audit"
+    exit 0
+fi
+drush ac --html --detail > /tmp/site_audit.html && firefox /tmp/site_audit.html > /dev/null 2>&1
